d54cba39f894f0894dde039baf24ffdc78687b47
dimitrmo <dmouratidis153@gmail.com>
2024-02-25 09:47:31 +0200

    Optimize size

--- src/project.rs
+++ src/project.rs
@@ -68,6 +68,7 @@ impl Project {
         Ok(())
     }
 
+    #[inline(always)]
     fn find_last_commit(repo: &Repository) -> std::result::Result<git2::Commit, git2::Error> {
         let obj = repo.head()?.resolve()?.peel(git2::ObjectType::Commit)?;
         obj.into_commit().map_err(|_| git2::Error::from_str("Couldn't find commit"))
